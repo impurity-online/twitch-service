a78c9f5abe46004994f3674c843e6e1e066d259c
Tyler Kokoszka <techkoko77@gmail.com>
2019-12-26 22:18:11 -0500

    updated config

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,13 @@ module.exports = {
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    coverageReporters: ['json', 'text', 'lcov', 'clover'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: -10,
+        },
+    },
 };
