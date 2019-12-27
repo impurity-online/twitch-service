b36a863c26789c1f0f6d0e765ad62ae5ef0a0e01
Tyler Kokoszka <techkoko77@gmail.com>
2019-12-26 23:26:25 -0500

    update environment configuration

--- src/environment.ts
+++ src/environment.ts
@@ -1,12 +1,26 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-interface IEnvironment {
-    environment: string | undefined;
-    port: string | undefined;
+export function getEnvironment(key: string, fallback?: string): string {
+    const value: string | undefined = process.env[key];
+    if (value) {
+        return value;
+    } else if (fallback) {
+        console.warn(`No environment variable ${key} found. Using ${fallback} instead`);
+        return fallback;
+    } else {
+        throw new Error(`No environment variable ${key} found and no explicit fallback supplied!`);
+    }
 }
 
-export default {
-    environment: process.env.NODE_ENV,
-    port: process.env.PORT,
-} as IEnvironment;
+interface IEnvironment {
+    nodeEnv: string;
+    port: string;
+}
+
+const environment: IEnvironment = {
+    nodeEnv: getEnvironment('NODE_ENV', 'local'),
+    port: getEnvironment('PORT', '8080'),
+};
+
+export default environment;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
-import env from './environment';
+import environment from './environment';
 import express, { Request, Response } from 'express';
+var fs = require('fs');
+var https = require('https');
 
 const app = express();
-const port = env.port;
+const port = environment.port;
 
-app.get('/', (request: Request, response: Response) => {
-    console.log(request.body);
-    console.log(response.status);
-    response.send('Hello World!');
-});
+if (environment.node_env === 'local') {
+    https
+        .createServer(
+            {
+                key: fs.readFileSync('server.key'),
+                cert: fs.readFileSync('server.cert'),
+            },
+            app,
+        )
+        .listen(3000, () => console.log(`Example app listening on port ${port}!`));
+} else {
+    app.get('/', (request: Request, response: Response) => {
+        console.log(request.body);
+        console.log(response.status);
+        response.send('Hello World!');
+    });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+}
