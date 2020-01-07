ab89499cf27710cb49fc468bd963ff55d4338f30
Tyler Kokoszka <techkoko77@gmail.com>
2020-01-06 19:33:53 -0500

    Update struct

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-    roots: ['<rootDir>/tests'],
-    preset: 'ts-jest',
+    roots: ['<rootDir>/test'],
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.json',
+        },
+    },
+    moduleFileExtensions: ['ts', 'js'],
+    transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
+    testMatch: ['**/test/**/*.test.(ts|js)'],
     testEnvironment: 'node',
     coverageReporters: ['json', 'text', 'lcov', 'clover'],
     coverageThreshold: {
